09acda2a173e76863972d832f8b7cecf53f98331
theminor <theminor@gmail.com>
2013-12-29 22:58:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,74 @@
 var Device = require('./lib/device')
   , util = require('util')
-  , stream = require('stream')
-  , configHandlers = require('./lib/config-handlers');
+  , stream = require('stream');
+
+// Give our module a stream interface
+util.inherits(myModule,stream);
+
+/**
+ * Called when our client starts up
+ * @constructor
+ *
+ * @param  {Object} opts Saved/default module configuration
+ * @param  {Object} app  The app event emitter
+ * @param  {String} app.id The client serial number
+ *
+ * @property  {Function} save When called will save the contents of `opts`
+ * @property  {Function} config Will be called when config data is received from the cloud
+ *
+ * @fires register - Emit this when you wish to register a device (see Device)
+ * @fires config - Emit this when you wish to send config data back to the cloud
+ */
+function myModule(opts,app) {
+
+  var self = this;
+
+  app.on('client::up',function(){
+
+    // The client is now connected to the cloud
+
+    // Do stuff with opts, and then commit it to disk
+    if (!opts.hasMutated) {
+      opts.hasMutated = true;
+    }
+
+    self.save();
+
+    // Register a device
+    self.emit('register', new Device());
+  });
+};
+
+/**
+ * Called when config data is received from the cloud
+ * @param  {Object} config Configuration data
+ */
+myModule.prototype.config = function(config) {
+
+};
+
+// Export it
+module.exports = myModule;
+
+
+
+// old version below
+
+//var Device = require('./lib/device')
+//  , util = require('util')
+//  , stream = require('stream')
+//  , configHandlers = require('./lib/config-handlers');
 
 // Give our driver a stream interface
-util.inherits(myDriver,stream);
+//util.inherits(myDriver,stream);
 
 // Our greeting to the user.
-var HELLO_WORLD_ANNOUNCEMENT = {
-  "contents": [
-    { "type": "heading",      "text": "Hello World Driver Loaded" },
-    { "type": "paragraph",    "text": "The hello world driver has been loaded. You should not see this message again." }
-  ]
-};
+//var HELLO_WORLD_ANNOUNCEMENT = {
+//  "contents": [
+//    { "type": "heading",      "text": "Hello World Driver Loaded" },
+//    { "type": "paragraph",    "text": "The hello world driver has been loaded. You should not see this message again." }
+//  ]
+//};
 
 /**
  * Called when our client starts up
@@ -28,26 +84,26 @@ var HELLO_WORLD_ANNOUNCEMENT = {
  * @fires register - Emit this when you wish to register a device (see Device)
  * @fires config - Emit this when you wish to send config data back to the Ninja Platform
  */
-function myDriver(opts,app) {
+//function myDriver(opts,app) {
 
-  var self = this;
+//  var self = this;
 
-  app.on('client::up',function(){
+//  app.on('client::up',function(){
 
     // The client is now connected to the Ninja Platform
 
     // Check if we have sent an announcement before.
     // If not, send one and save the fact that we have.
-    if (!opts.hasSentAnnouncement) {
-      self.emit('announcement',HELLO_WORLD_ANNOUNCEMENT);
-      opts.hasSentAnnouncement = true;
-      self.save();
-    }
+//    if (!opts.hasSentAnnouncement) {
+//      self.emit('announcement',HELLO_WORLD_ANNOUNCEMENT);
+//      opts.hasSentAnnouncement = true;
+//      self.save();
+//    }
 
     // Register a device
-    self.emit('register', new Device());
-  });
-};
+//    self.emit('register', new Device());
+//  });
+//};
 
 /**
  * Called when a user prompts a configuration.
@@ -59,23 +115,23 @@ function myDriver(opts,app) {
  * @param  {Object}   rpc.params Any input data the user provided
  * @param  {Function} cb      Used to match up requests.
  */
-myDriver.prototype.config = function(rpc,cb) {
+//myDriver.prototype.config = function(rpc,cb) {
 
-  var self = this;
+//  var self = this;
   // If rpc is null, we should send the user a menu of what he/she
   // can do.
   // Otherwise, we will try action the rpc method
-  if (!rpc) {
-    return configHandlers.menu.call(this,cb);
-  }
-  else if (typeof configHandlers[rpc.method] === "function") {
-    return configHandlers[rpc.method].call(this,rpc.params,cb);
-  }
-  else {
-    return cb(true);
-  }
-};
+//  if (!rpc) {
+//    return configHandlers.menu.call(this,cb);
+//  }
+//  else if (typeof configHandlers[rpc.method] === "function") {
+//    return configHandlers[rpc.method].call(this,rpc.params,cb);
+//  }
+//  else {
+//    return cb(true);
+//  }
+//};
 
 // Export it
-module.exports = myDriver;
+//module.exports = myDriver;
 
