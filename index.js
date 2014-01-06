b0cf8d8137183774a8a1dca3a2975bc885ea1ed7
theminor <theminor@gmail.com>
2014-01-06 17:31:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ util.inherits(Device,stream);
 
 // not so elegant way to store all of the devices created by the driver, plus a few variables...
 var deviceList = [];
-var updateInterval = 300000; // update interval in milliseconds (for example, 600000 = 10 minutes)
-var pauseAfterSetToUpdate = 7000; // time in milliseconds to wait after submitting/setting data before we try to run an update
-var pauseBetweenUpdateCommands = 5; // time in seconds to wait between submitting/setting the two data requests - this is not currently used...
-var ipAddressOfThermostat = "192.168.1.111"; // ip address of the thermostat
+var weathDataFeatures = ["conditions"]  // a single api request can combine many forms of data so as to save you # of requests per day, etc. - can be: alerts, almanac, astronomy, conditions, currenthurricane, forecast, forecast10day, geolookup, history, hourly, hourly10day, planner, rawtide, satellite, tide, webcams, yesterday
+var apiKey = "xxx"; // api key obtained from http://www.wunderground.com/weather/api/ (documentation: http://www.wunderground.com/weather/api/d/docs)
+var zipCode = "10007"; // zip code of location
+var useFahrenheit = true; // set false to use Celsius
+var pauseAfterSetToUpdate = 5000; // in milliseconds
+var updateInterval = 300000; // in milliseconds
 
 function Driver(opts,app) {
 	this._app = app;
 	this.opts = opts;
-	if (opts.ipadr) ipAddressOfThermostat = opts.ipadr; // ugly way to track these, but it should work for now...
-	if (opts.updtInt) updateInterval = opts.updtInt;
-	if (opts.pauseAftUpdt) pauseAfterSetToUpdate = opts.pauseAftUpdt;
+	if (opts.apiKey) apiKey = opts.apiKey; // ugly way to track these, but it should work for now...
+	if (opts.zipCode) zipCode = opts.zipCode;
+	if (opts.useFahrenheit) useFahrenheit = opts.useFahrenheit;	
+	if (opts.pauseAfterSetToUpdate) pauseAfterSetToUpdate = opts.pauseAfterSetToUpdate;	
+	if (opts.updateInterval) updateInterval = opts.updateInterval;		
 	app.once('client::up',function(){
 		commands.forEach( this.createCommandDevice.bind(this) );
 		updateDevices(app, opts);
@@ -37,49 +41,51 @@ Driver.prototype.createCommandDevice = function(cmd) {
 };
 
 function Device(app, config) {
-	app.log.info('Creating radioThermostatDriver Device : ' + config.name);
+	app.log.info('Creating weatherDriver Device : ' + config.name);
 	var self = this;
 	this._app = app;
 	this.config = config;
 	this.readable = true;
 	this.writeable = config.canSet || false;
-	if (this.writeable) { app.log.info('radioThermostatDriver Device ' + config.name + ' is readable and writable' ); } else app.log.info('radioThermostatDriver Device ' + config.name + ' is readable only' );
+	if (this.writeable) { app.log.info('weatherDriver Device ' + config.name + ' is readable and writable' ); } else app.log.info('weatherDriver Device ' + config.name + ' is readable only' );
 	this.V = config.vendorId || 0;
 	this.D = config.deviceId;
-	this.G = 'rtd' + (config.name).replace(/[^a-zA-Z0-9]/g, '');
-	this.name = 'radioThermostatDriver - ' + config.name;
+	this.G = 'wd' + (config.name).replace(/[^a-zA-Z0-9]/g, '');
+	this.name = 'weatherDriver - ' + config.name;
 	// this.read();
 };
 
 function updateDevices(app, opts) {	// runs every "updateInterval" seconds
-	app.log.info("Updating radioThermostatDriver Devices...");
-	var tstatCmd = "curl -s http://" + ipAddressOfThermostat + "/tstat";
-	app.log.info("radioThermostatDriver executing command: " + tstatCmd);
-	exec(tstatCmd, function(error, stdout, stderr) {
-		app.log.info("Result of radioThermostatDriver command: " + stdout);
+	app.log.info("Updating weatherDriver Devices...");
+	var getTempCmd = "curl -s http://api.wunderground.com/api/" + apiKey + "/" + weathDataFeatures.join("/") + "/q/" + zipCode + ".json";  // example: http://api.wunderground.com/api/6ed6c7fe07d64fa7/conditions/q/30736.json - See api for documentation - http://api.wunderground.com/api/apiKey/features (can be combined more than one)/settings (leave out to accept defaults)/q/query (the location - can be a zip code, city, etc).format (json or xml)
+	app.log.info("weatherDriver executing command: " + getTempCmd);
+	exec(getTempCmd, function(error, stdout, stderr) {
+		app.log.info("Result of weatherDriver command: " + stdout);
 		if (error) {
 			if (error != null) {
-				app.log.warn('radioThermostatDriver : ' + this.name + ' error! - ' + error);
+				app.log.warn('weatherDriver : ' + this.name + ' error! - ' + error);
 			};
 		}
 		else if (stderr) {
 			if (stderr != null) {
-				app.log.warn('radioThermostatDriver : ' + this.name + ' stderr! - ' + stderr);
+				app.log.warn('weatherDriver : ' + this.name + ' stderr! - ' + stderr);
 			};
 		}
 		else {
+			var useFht = false;
+			if (useFahrenheit==true || useFahrenheit=="true") { useFht = true }; // account for "false" stored as string
 			var inputString = (stdout + '');
-			var thermostatData = eval ("(" + inputString + ")");
-			if (!thermostatData) {
-				app.log.warn('radioThermostatDriver was unable to parse data recieved. No update was made this cycle.');
+			var wData = eval ("(" + inputString + ")");
+			if (!wData) {
+				app.log.warn('weatherDriver was unable to parse data recieved. No update was made this cycle.');
 			}
 			else {
 				deviceList.forEach(function(dev){
-					app.log.info('Updating radioThermostatDriver Device: ' + dev.name);
+					app.log.info('Updating weatherDriver Device: ' + dev.name);
 					var parsedResult = undefined;
 					(dev.config.data || []).forEach(function(fn) {
 						try {
-							parsedResult = fn(thermostatData);
+							parsedResult = fn(wData, useFht);
 						} catch(e) {
 							parsedResult = undefined;
 						}
@@ -101,18 +107,18 @@ function updateDevices(app, opts) {	// runs every "updateInterval" seconds
 Device.prototype.write = function(dataRcvd) {
 	var app = this._app;
 	var opts = this.opts;
-	app.log.info("radioThermostatDriver Device " + this.name + " received data: " + dataRcvd);
-	app.log.info("radioThermostatDriver Device canSet: " + this.config.canSet);
+	app.log.info("weatherDriver Device " + this.name + " received data: " + dataRcvd);
+	app.log.info("weatherDriver Device canSet: " + this.config.canSet);
 	if (this.config.canSet) {
 		var stgSubmit = undefined;
 		(this.config.setStg || []).forEach(function(fn) {
 			try {
-				stgSubmit = fn(ipAddressOfThermostat, dataRcvd);
+				stgSubmit = fn(apiKey, dataRcvd);
 			} catch(e) {
 				stgSubmit = undefined;
 			}
 		});
-		app.log.info("radioThermostatDriver string: " + stgSubmit);
+		app.log.info("weatherDriver string: " + stgSubmit);
 		if (stgSubmit !== undefined) {
 			app.log.info(this.name + " - submitting data to thermostat: " + stgSubmit);
 			var rslt = exec(stgSubmit, function (error, stdout, stderr) {
@@ -126,21 +132,22 @@ Device.prototype.write = function(dataRcvd) {
 		};		
 	}
 	else {
-		app.log.info("radioThermostatDriver Device " + this.name + " received data, but this type of device can't update");
+		app.log.info("weatherDriver Device " + this.name + " received data, but this type of device can't update");
 	}
 };
 
 Driver.prototype.config = function(rpc,cb) {
 	var self = this;
 	if (!rpc) {
-		this._app.log.info("radioThermostatDriver main config window called");
+		this._app.log.info("weatherDriver main config window called");
 		return cb(null, {	// main config window
 			"contents":[
-				{ "type": "paragraph", "text": "The radioThermostatDriver allows you to monitor and control WiFi Thermostats such as the ones from radiothermostat.com. Enter the settings below to get started, and please make sure you get a confirmation message after hitting 'Submit' below. (You may have to click it a couple of times. If you don't get a confirmation message, the settings did not update!)"},
-				{ "type": "input_field_text", "field_name": "ip_addr_text", "value": "", "label": "IP Address of Your Thermostat", "placeholder": ipAddressOfThermostat, "required": true},
-				{ "type": "input_field_text", "field_name": "update_int_secs_text", "value": "", "label": "Update Interval in Seconds", "placeholder": updateInterval/1000, "required": true},
-				{ "type": "input_field_text", "field_name": "pause_bt_cmds_secs_text", "value": "", "label": "Seconds to Pause Between Thermostat Commands", "placeholder": pauseBetweenUpdateCommands, "required": true},
-				{ "type": "input_field_text", "field_name": "pause_aft_updt_secs_text", "value": "", "label": "Seconds to Pause After a Command Before Updating", "placeholder": pauseAfterSetToUpdate/1000, "required": true},
+				{ "type": "paragraph", "text": "The weatherDriver allows you to monitor the weather outside. To use this, you'll need a free api from http://www.wunderground.com/weather/api/ - Enter the settings below to get started, and please make sure you get a confirmation message after hitting 'Submit' below. (You may have to click it a couple of times. If you don't get a confirmation message, the settings did not update!)"},
+				{ "type": "input_field_text", "field_name": "api_text", "value": apiKey, "label": "API from wunderground.com", "placeholder": apiKey, "required": true},
+				{ "type": "input_field_text", "field_name": "zip_code_text", "value": zipCode, "label": "Zip Code", "placeholder": zipCode, "required": true},
+				{ "type": "input_field_select", "field_name": "use_fahrenheit_select", "label": "Temperature Type to Display", "options": [{ "name": "Fahrenheit", "value": true, "selected": useFahrenheit}, { "name": "Celsius", "value": false, "selected": !useFahrenheit}], "required": true },
+				{ "type": "input_field_text", "field_name": "pause_aft_updt_secs_text", "value": pauseAfterSetToUpdate/1000, "label": "Seconds to Pause After a Command Before Updating", "placeholder": pauseAfterSetToUpdate/1000, "required": true},
+				{ "type": "input_field_text", "field_name": "update_interval_text", "value": updateInterval/1000, "label": "How frequently to update data in seconds. (NOTE each update counts as an api call, so limit this per the number of calls per day your api plan allows)", "placeholder": updateInterval/1000, "required": true},
 				{ "type": "paragraph", "text": " "},
 				{ "type": "submit", "name": "Submit", "rpc_method": "submt" },
 				{ "type": "close", "name": "Cancel" },
@@ -148,37 +155,18 @@ Driver.prototype.config = function(rpc,cb) {
 		});
 	};
 	if (rpc.method == "submt") {
-		this._app.log.info("radioThermostatDriver config window submitted. Checking data for errors...");
+		this._app.log.info("weatherDriver config window submitted. Checking data for errors...");
 		// check for errors
-		var rgx = new RegExp("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"); // Test ip_addr_text to match this regex - simple ip address checking does not check nodes for > 255, but should suffice for now...
-		if (!rgx.test(rpc.params.ip_addr_text)) {
+		if (!(rpc.params.zip_code_text >= 0)) {	// zip_code_text must evaluate to a positive number or 0
 			cb(null, {
 				"contents": [
-					{ "type": "paragraph", "text": "IP address was invalid - please use standard ipv4 address, such as: 123.45.6.789. Please try again." },
-					{ "type": "close"    , "name": "Close" }
-				]
-			});			
-			return;
-		}
-		else if (!(rpc.params.update_int_secs_text >= 0)) {	// update_int_secs_text must evaluate to a positive number 
-			cb(null, {
-				"contents": [
-					{ "type": "paragraph", "text": "the 'update interval' must be a number and can't be negative. Please try again." },
+					{ "type": "paragraph", "text": "zip code must be a number and can't be negative. Please try again." },
 					{ "type": "close"    , "name": "Close" }
 				]
 			});			
 			return;			
 		}
-		else if (!(rpc.params.pause_bt_cmds_secs_text >= 0)) {	// pause_bt_cmds_secs_text must evaluate to a positive number 
-			cb(null, {
-				"contents": [
-					{ "type": "paragraph", "text": "The 'pause between commands' interval must be a number and can't be negative. Please try again." },
-					{ "type": "close"    , "name": "Close" }
-				]
-			});			
-			return;				
-		}	
-		else if (!(rpc.params.pause_aft_updt_secs_text >= 0)) {	// pause_aft_updt_secs_text must evaluate to a positive number 
+		else if (!(rpc.params.pause_aft_updt_secs_text >= 0)) {	// pause_aft_updt_secs_text must evaluate to a positive number or 0
 			cb(null, {
 				"contents": [
 					{ "type": "paragraph", "text": "The 'pause after update' interval must be a number and can't be negative. Please try again." },
@@ -187,19 +175,31 @@ Driver.prototype.config = function(rpc,cb) {
 			});			
 			return;				
 		}
+		else if (!(rpc.params.update_interval_text >= 0)) {	// update_interval_text must evaluate to a positive number or 0
+			cb(null, {
+				"contents": [
+					{ "type": "paragraph", "text": "The 'update interval' must be a number and can't be negative. Please try again." },
+					{ "type": "close"    , "name": "Close" }
+				]
+			});			
+			return;				
+		}		
 		else {	// looks like the submitted values were valid, so update
-			this._app.log.info("radioThermostatDriver data appears valid. Saving settings...");
-            self.opts.ipadr = rpc.params.ip_addr_text;
-            self.opts.updtInt = rpc.params.update_int_secs_text * 1000; // need this in milliseconds
-			self.opts.pauseBtCmds = rpc.params.pause_bt_cmds_secs_text; // this optin isn't used right now...
+			this._app.log.info("weatherDriver data appears valid. Saving settings...");
+            self.opts.apiKey = rpc.params.api_text;
+            self.opts.zipCode = rpc.params.zip_code_text;
+			self.opts.useFahrenheit = rpc.params.use_fahrenheit_select;
 			self.opts.pauseAftUpdt = rpc.params.pause_aft_updt_secs_text * 1000; // also need this in milliseconds
-			ipAddressOfThermostat = self.opts.ipadr; // ugly way to track these, but it should work for now...
-			updateInterval = self.opts.updtInt;
-			pauseAfterSetToUpdate = self.opts.pauseAftUpdt;
+			self.opts.updateInterval = rpc.params.update_interval_text * 1000; // also need this in milliseconds			
+			apiKey = self.opts.apiKey; // ugly way to track these, but it should work for now...
+			zipCode = self.opts.zipCode;
+			useFahrenheit = self.opts.useFahrenheit;
+			pauseAftUpdt = self.opts.pauseAftUpdt;
+			updateInterval = self.opts.updateInterval;			
 			self.save();
 			cb(null, {
 				"contents": [
-					{ "type": "paragraph", "text": "Configuration was successful. radioThermostatDriver values should update shortly!" },
+					{ "type": "paragraph", "text": "Configuration was successful. weatherDriver values should update shortly!" },
 					{ "type": "close"    , "name": "Close" }
 				]
 			});
@@ -207,9 +207,8 @@ Driver.prototype.config = function(rpc,cb) {
 		};
 	}
 	else {
-		this._app.log.info("radioThermostatDriver - Unknown rpc method was called!");
+		this._app.log.info("weatherDriver - Unknown rpc method was called!");
 	};
 };
 
 module.exports = Driver;
-
