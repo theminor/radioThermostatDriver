d4f87d4c19919768fb05ee8f0deacd32f1bda96a
theminor <theminor@gmail.com>
2014-01-07 10:49:09 -0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -2,7 +2,7 @@ module.exports = [
     {
         name: 'Thermostat Current Temperature',
         deviceId: 9,
-        data: [function(thermostatData) {
+        getStg: [function(thermostatData) {
 			return thermostatData.temp;
         }],
 		canSet: false,
@@ -10,46 +10,67 @@ module.exports = [
     {
         name: 'Thermostat Target Temperature',
         deviceId: 31,
-        data: [function(thermostatData) {
+        getStg: [function(thermostatData) {
 			var tTemp = thermostatData.t_cool;
 			if (!tTemp) tTemp = thermostatData.t_heat;
 			return tTemp;
         }],
 		canSet: true,
-		setStg: [function(ipAddr, val) { // example:   curl -d '{"t_cool":67,"t_heat":67}' http://1.1.1.1/tstat
-			if (isNaN(val)) return ""; else return "curl -d '{\"t_cool\":" + val + ",\"t_heat\":" + val + "}' http://" + ipAddr + "/tstat";	// setStg just returns the string we want to execute
+		setStg: [function(ipAddr, data, lastData) { // example:   curl -d '{"t_cool":67,"t_heat":67}' http://1.1.1.1/tstat
+			if (isNaN(data)) {
+				return undefined;
+			}
+			else {
+				if (lastData.t_heat) { return "curl -d '{\"t_heat\":" + data + "}' http://" + ipAddr + "/tstat"; }
+				else if (lastData.t_cool) { return "curl -d '{\"t_cool\":" + data + "}' http://" + ipAddr + "/tstat"; }
+				else { return undefined; }
+			}
+			// if (isNaN(data)) return ""; else return "curl -d '{\"t_cool\":" + data + ",\"t_heat\":" + data + "}' http://" + ipAddr + "/tstat";        // setStg just returns the string we want to execute
         }],
     },
     {
-        name: 'Thermostat Operating Mode',		// current thermostat operating mode ("tmode") - will be one of: 0: OFF, 1: HEAT, 2: COOL, 3: AUTO
+        name: 'Thermostat Hold Mode',                // current thermostat hold mode ("tmode") - will be one of: 0: disabled, 1: enabled
         deviceId: 244,
-        data: [function(thermostatData) {
+        getStg: [function(thermostatData) {
+			var hmModes = ["Thermostat Hold Disabled","Thermostat Hold Enabled"];
+			return hmModes[thermostatData.tmode] || "Thermostat Hold Unknown";
+        }],
+		canSet: true,
+		setStg: [function(ipAddr, data, lastData) { // example:   curl -d '{"hold":1}' http://1.1.1.1/tstat  (turns hold on)
+			var hldNum = {"Thermostat Hold Disabled":0,"Thermostat Hold Enabled":1};
+			if (hldNum[data]) { return "curl -d '{\"hold\":" + hldNum[mode] + "}' http://" + ipAddr + "/tstat"; } else { return undefined; };
+        }],
+    },
+    {
+        name: 'Thermostat Operating Mode',                // current thermostat operating mode ("tmode") - will be one of: 0: OFF, 1: HEAT, 2: COOL, 3: AUTO
+        deviceId: 244,
+        getStg: [function(thermostatData) {
 			var opModes = ["Thermostat Mode Off","Thermostat Heat Mode","Thermostat Cool Mode","Thermostat Auto Mode"];
 			return opModes[thermostatData.tmode] || "Thermostat Mode Unknown";
         }],
 		canSet: true,
-		setStg: [function(ipAddr, mode) { // example:   curl -d '{"tmode":1}' http://1.1.1.1/tstat  (turns to heat mode)
+		setStg: [function(ipAddr, data, lastData) { // example:   curl -d '{"tmode":1}' http://1.1.1.1/tstat  (turns to heat mode)
 			var modNum = {"Thermostat Mode Off":0,"Thermostat Heat Mode":1,"Thermostat Cool Mode":2,"Thermostat Auto Mode":3};
-			if (modNum[mode]) return "curl -d '{\"tmode\":" + modNum[mode] + "}' http://" + ipAddr + "/tstat"; else return "";
+			if (modNum[data]) { return "curl -d '{\"tmode\":" + modNum[data] + "}' http://" + ipAddr + "/tstat"; } else { return undefined; };
         }],
     },
     {
-        name: 'Thermostat Fan Operating Mode',		// show current thermostat fan operating mode ("fmode") - will be one of: 0: AUTO, 1: AUTO/CIRCULATE, 2: ON
+        name: 'Thermostat Fan Operating Mode',                // show current thermostat fan operating mode ("fmode") - will be one of: 0: AUTO, 1: AUTO/CIRCULATE, 2: ON
         deviceId: 244,
-        data: [function(thermostatData) {
+        getStg: [function(thermostatData) {
 			var fanOpModes = ["Thermostat Fan Auto Mode","Thermostat Fan Auto/Circ","Thermostat Fan Mode On"];
 			return fanOpModes[thermostatData.fmode] || "Thermostat Fan Unknown";
         }],
 		canSet: true,
-		setStg: [function(ipAddr, mode) { // example:   curl -d '{"tmode":1}' http://1.1.1.1/tstat  (turns to heat mode)
+		setStg: [function(ipAddr, data, lastData) { // example:   curl -d '{"tmode":1}' http://1.1.1.1/tstat  (turns to heat mode)
 			var modNum = {"Thermostat Fan Auto Mode":0,"Thermostat Fan Auto/Circ":1,"Thermostat Fan Mode On":2};
-			if (modNum[mode]) return "curl -d '{\"tmode\":" + modNum[mode] + "}' http://" + ipAddr + "/tstat"; else return "";
+			if (modNum[data]) { return "curl -d '{\"tmode\":" + modNum[data] + "}' http://" + ipAddr + "/tstat"; } else { return undefined; };
         }],
-    },	
+    },        
     {
-        name: 'Thermostat Time',		// output a string of the thermostat time ("time":{"day":0,"hour":0,"minute":0}}) - day 0 = Monday
+        name: 'Thermostat Time',                // output a string of the thermostat time ("time":{"day":0,"hour":0,"minute":0}}) - day 0 = Monday
         deviceId: 3680,
-        data: [function(thermostatData) {
+        getStg: [function(thermostatData) {
 			var daysWeek = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday', 'Sunday'];
 			var weekDay = daysWeek[thermostatData.time.day];
 			var paddedMin = (thermostatData.time.minute < 10) ? ("0" + thermostatData.time.minute) : thermostatData.time.minute;
@@ -60,9 +81,11 @@ module.exports = [
 ];
 
 /*
-	name: string - the name of the device
-	deviceId: number - the device id of the device - see http://ninjablocks.com/pages/device-ids
-	data: function to call which will return parsed date when given ((json object)thermostatData) as arguments
-	canSet: boolean - set to true if device is writable
-	setStg: a function to call if device is writable upon write which will return a string of the command to execute, given ((string)ipAddr, (string)data) as arguments
+    name: string - the name of the device
+    deviceId: number - the device id of the device - see http://ninjablocks.com/pages/device-ids
+    data: function to call which will return parsed date when given ((json object)thermostatData) as arguments
+    canSet: boolean - set to true if device is writable
+    setStg: a function to call if device is writable upon write which will return a string of the command to execute, given ((string)ipAddr, (string)data), (json object)lastData as arguments
+	
+	example retuned data: {"temp":65.50,"tmode":1,"fmode":0,"override":0,"hold":0,"t_heat":68.00,"tstate":1,"fstate":1,"time":{"day":1,"hour":10,"minute":9},"t_type_post":0}
 */
