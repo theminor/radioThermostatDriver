0af5e9060dbb43e342ccbfc7d286faabfda4aac7
theminor <theminor@gmail.com>
2014-02-15 11:34:18 -0500

    Major re-write; add ability to have multiple thermostats; fix settings

--- index.js
+++ index.js
@@ -48,12 +48,10 @@ Driver.prototype.establishDevices = function() {
 
 Driver.prototype.updateDevices = function() {
 	var self = this;
-	self._app.log.info("Updating radioThermostatDriver Devices...");
-//	self._app.log.debug("Updating radioThermostatDriver Devices...");
+	self._app.log.debug("Updating radioThermostatDriver Devices...");
 	for (ip in self.ips) {	// fetch the data once per ip address, then use that data and parse many times as necessary per device
 		if (self.ips[ip]) {	// cover the weird case of an undefined key in the object (perhaps in case one got removed by changing settings, for example)
-			self._app.log.info("radioThermostatDriver connecting to: http://" + ip + "/tstat");
-//			self._app.log.debug("radioThermostatDriver connecting to: http://" + ip + "/tstat");
+			self._app.log.debug("radioThermostatDriver connecting to: http://" + ip + "/tstat");
 			http.get(			// see http://nodejs.org/docs/v0.5.2/api/http.html#http.get
 			{	host: ip,		// example would be "curl -s http://" + ipAddressOfThermostat + "/tstat"
 				port: 80,
@@ -66,8 +64,7 @@ Driver.prototype.updateDevices = function() {
 				});
 				res.on('end', function () {
 					var inputString = (result + '');
-					self._app.log.info("Result of radioThermostatDriver command: " + inputString);
-//					self._app.log.debug("Result of radioThermostatDriver command: " + inputString);
+					self._app.log.debug("Result of radioThermostatDriver command: " + inputString);
 					var tstatData = eval ("(" + inputString + ")"); // return data into var tstatData
 					if (!tstatData) {
 						self._app.log.warn('radioThermostatDriver was unable to parse data recieved. No update was made this cycle.');
@@ -82,17 +79,14 @@ Driver.prototype.updateDevices = function() {
 								} catch(e) {
 									parsedResult = undefined;
 								}
-								self._app.log.info(device.name + ' - parse data: ' + inputString + ' --> ' + parsedResult);
-//								self._app.log.debug(device.name + ' - parse data: ' + inputString + ' --> ' + parsedResult);
+								self._app.log.debug(device.name + ' - parse data: ' + inputString + ' --> ' + parsedResult);
 							});
 							if (parsedResult !== undefined) {
-								self._app.log.info(device.name + ' - emmitting data: ' + parsedResult);
-//								self._app.log.debug(device.name + ' - emmitting data: ' + parsedResult);
+								self._app.log.debug(device.name + ' - emmitting data: ' + parsedResult);
 								device.emit('data', parsedResult);
 							}
 							else {
-								self._app.log.info(device.name + ' - did not emmit data!');
-//								self._app.log.debug(device.name + ' - did not emmit data!');
+								self._app.log.debug(device.name + ' - did not emmit data!');
 							};
 						});
 					};
@@ -111,8 +105,7 @@ function Device(app, config, ip) {
 	this.config.ip = ip;
 	this.readable = true;
 	this.writeable = config.canSet || false;
-	if (this.writeable) { app.log.info('radioThermostatDriver Device ' + config.name + ' is readable and writable' ); } else app.log.debug('radioThermostatDriver Device ' + config.name + ' is readable only' );
-//	if (this.writeable) { app.log.debug('radioThermostatDriver Device ' + config.name + ' is readable and writable' ); } else app.log.debug('radioThermostatDriver Device ' + config.name + ' is readable only' );
+	if (this.writeable) { app.log.debug('radioThermostatDriver Device ' + config.name + ' is readable and writable' ); } else app.log.debug('radioThermostatDriver Device ' + config.name + ' is readable only' );
 	this.V = config.vendorId || 0;
 	this.D = config.deviceId;
 	this.G = 'rtd' + (config.name + ip).replace(/[^a-zA-Z0-9]/g, '');
@@ -121,8 +114,7 @@ function Device(app, config, ip) {
 
 Device.prototype.write = function(dataRcvd) {
 	var self = this;
-	this._app.log.info("radioThermostatDriver Device " + this.name + " received data: " + dataRcvd);
-//	this._app.log.debug("radioThermostatDriver Device " + this.name + " received data: " + dataRcvd);
+	this._app.log.debug("radioThermostatDriver Device " + this.name + " received data: " + dataRcvd);
 	if (this.config.canSet) {
 		var postData = undefined;
 		(this.config.setStg || []).forEach(function(fn) {
@@ -132,11 +124,9 @@ Device.prototype.write = function(dataRcvd) {
 				postData = undefined;
 			}
 		});
-		this._app.log.info("radioThermostatDriver string: " + postData);
-//		this._app.log.debug("radioThermostatDriver string: " + postData);
+		this._app.log.debug("radioThermostatDriver string: " + postData);
 		if (postData !== undefined) {
-			this._app.log.info(this.name + " - submitting data to thermostat: " + postData);
-//			this._app.log.debug(this.name + " - submitting data to thermostat: " + postData);
+			this._app.log.debug(this.name + " - submitting data to thermostat: " + postData);
 			var post_req = http.request(
 				{
 					host: this.config.ip,
@@ -154,8 +144,7 @@ Device.prototype.write = function(dataRcvd) {
 						result += chunk;
 					});
 					res.on('end', function () {
-						self._app.log.info(self.name + " - Result: " + result);
-//						self._app.log.debug(self.name + " - Result: " + result);
+						self._app.log.debug(self.name + " - Result: " + result);
 						setTimeout( function() { self.parentDevice.updateDevices() }, self.parentDevice.opts.pauseAftUpdt );
 					});
 				}
@@ -164,21 +153,18 @@ Device.prototype.write = function(dataRcvd) {
 			post_req.end();
 		}
 		else {
-			this._app.log.info(this.name + ' - error parsing data!');
-//			this._app.log.debug(this.name + ' - error parsing data!');
+			this._app.log.debug(this.name + ' - error parsing data!');
 		};                
 	}
 	else {
-		this._app.log.info("radioThermostatDriver Device " + this.name + " received data, but this type of device can't update");
-//		this._app.log.debug("radioThermostatDriver Device " + this.name + " received data, but this type of device can't update");
+		this._app.log.debug("radioThermostatDriver Device " + this.name + " received data, but this type of device can't update");
 	};
 };
 
 Driver.prototype.config = function(rpc, cb) {
 	var self = this;
 	if (!rpc) {
-		this._app.log.info("radioThermostatDriver main config window called");
-//		this._app.log.debug("radioThermostatDriver main config window called");
+		this._app.log.debug("radioThermostatDriver main config window called");
 		return cb(null, {        // main config window
 			"contents":[
 				{ "type": "paragraph", "text": "The radioThermostatDriver allows you to monitor and control WiFi Thermostats such as the ones from radiothermostat.com. Enter the settings below to get started, and please make sure you get a confirmation message after hitting 'Submit' below. (You may have to click it a couple of times. If you don't get a confirmation message, the settings did not update!)"},
@@ -193,19 +179,20 @@ Driver.prototype.config = function(rpc, cb) {
 		});
 	};
 	if (rpc.method == "submt") {
-		this._app.log.info("radioThermostatDriver config window submitted. Checking data for errors..."); // check for errors
-//		this._app.log.debug("radioThermostatDriver config window submitted. Checking data for errors..."); // check for errors
+		this._app.log.debug("radioThermostatDriver config window submitted. Checking data for errors..."); // check for errors
 		var rgx = new RegExp("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"); // Test ip_addr_text to match this regex - simple ip address checking does not check nodes for > 255, but should suffice for now...
-		if (!rgx.test(rpc.params.ip_addr_text)) {
-			cb(null, {
-				"contents": [
-					{ "type": "paragraph", "text": "IP address was invalid - please use standard ipv4 address, such as: 123.45.6.789. Please try again." },
-					{ "type": "close"    , "name": "Close" }
-				]
-			});                        
-			return;
+		rpc.params.ip_addr_text.split("|").forEach(function(ip) {
+			if (!rgx.test(ip.trim())) {
+				cb(null, {
+					"contents": [
+						{ "type": "paragraph", "text": 'IP address was invalid - please use standard ipv4 address, such as: 123.45.6.789. If using multiple ip addresses, selerate each with a pipe charagter ("|"). Do not put and spaces between them. Please try again.' },
+						{ "type": "close"    , "name": "Close" }
+					]
+				});                        
+				return;
+			}
 		}
-		else if (!(rpc.params.update_int_secs_text >= 0)) {        // update_int_secs_text must evaluate to a positive number 
+		if (!(rpc.params.update_int_secs_text >= 0)) {        // update_int_secs_text must evaluate to a positive number 
 			cb(null, {
 				"contents": [
 					{ "type": "paragraph", "text": "the 'update interval' must be a number and can't be negative. Please try again." },
@@ -233,8 +220,7 @@ Driver.prototype.config = function(rpc, cb) {
 			return;                                
 		}
 		else {        // looks like the submitted values were valid, so update
-			this._app.log.info("radioThermostatDriver data appears valid. Saving settings...");
-//			this._app.log.debug("radioThermostatDriver data appears valid. Saving settings...");
+			this._app.log.debug("radioThermostatDriver data appears valid. Saving settings...");
 			self.opts.ipadr = rpc.params.ip_addr_text;
 			self.opts.updtInt = rpc.params.update_int_secs_text * 1000; // need this in milliseconds
 			self.opts.pauseBtCmds = rpc.params.pause_bt_cmds_secs_text; // this optin isn't used right now...
